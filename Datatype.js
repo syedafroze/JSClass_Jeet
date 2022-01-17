34b4617e4813757aa4acc4421ddb1686ac7fc5c6
syedafroze <safroze112@gmail.com>
2022-01-17 11:07:14 +0530

    uploaded

---
+++ Datatype.js
@@ -0,0 +1,16 @@
+/*
+PRIMITIVE Datatype ()
+
+number 
+string 
+boolean 
+undefined 
+null
+symbol
+
+NON primitive (Reference datatype) 
+
+object   let user = {name : "syed", age:"25" , id:8}
+function  
+
+*/
---
+++ variable.js
@@ -0,0 +1,26 @@
+let num = 5 ;
+
+
+
+
+//scope - range 
+
+//local scope , block scope 
+
+//{ let num = 7 }
+
+// = assigment 
+
+// double equalvalent   
+//string "" '' 
+
+if(num == 5){
+   let num = 8;
+   console.log(num + 7 );//15 
+}
+
+const value = "25+5" ;//string
+
+
+console.log( typeof value);//12
+
